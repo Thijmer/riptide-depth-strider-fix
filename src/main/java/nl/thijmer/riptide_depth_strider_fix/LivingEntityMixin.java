31aea1d9b23ded65f8de4316ed0a06c8ac2e0750
Thijmer <thijmenvoskuilen@gmail.com>
2025-12-21 14:49:59 +0100

    Update gradle, migrate to Mojang mappings, update to 1.21.11

--- src/main/java/nl/thijmer/riptide_depth_strider_fix/LivingEntityMixin.java
+++ src/main/java/nl/thijmer/riptide_depth_strider_fix/LivingEntityMixin.java
@@ -1,7 +1,7 @@
 package nl.thijmer.riptide_depth_strider_fix;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,17 +9,17 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Shadow protected int riptideTicks;
+    @Shadow protected int autoSpinAttackTicks;
 
     // Inject this function at the right point in the travelInFluid function.
     @ModifyVariable(
-            method = "travelInFluid(Lnet/minecraft/util/math/Vec3d;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getMovementSpeed()F"),
+            method = "travelInWater(Lnet/minecraft/world/phys/Vec3;DZD)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getSpeed()F"),
             ordinal = 0
     )
     protected float undoRiptideDrag(float f, @Local(ordinal = 2) float h) {
         // Undo the drag increase from depth strider when we're using Riptide.
-        if (this.riptideTicks != 0 && (1 - h) != 0) {
+        if (this.autoSpinAttackTicks != 0 && (1 - h) != 0) {
             return (f - 0.54600006F * h) / (1 - h);
         }
         return f;
